e3fd68f4e26bec43dbfd6d9ab6080f084cd16b9c
Kristian Portillo <portillk@gmail.com>
2020-07-07 19:21:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,11 @@ def new_hash
 end
 
 def my_hash
-  {my_hash:"is_cool"# return a valid hash with any key/value pair of your choice
+  {my_hash:"is_cool"}# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
- {name: "Grace Hopper" # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+ {name: "Grace Hopper"} # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
